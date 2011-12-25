d1d7ff944ababd8d16212f2083f4dfc462e33ccd
Zach Fogg <zach@zachfogg.com>
2011-12-24 23:42:05 -0500

    Attempts at parallelism.

--- projectEuler21.hs
+++ projectEuler21.hs
@@ -4,12 +4,13 @@ import Data.List
 import Data.Maybe
 import Control.Parallel.Strategies
 import Control.Parallel
+import Euler
 
 amicablePairs = [ (x, y) | (x, y) <- uniquePairs [1..10^4], (sigma x) - x == y && (sigma y) - y == x ]
 
 -- Euler's Sigmas.
 -- When n is prime, these functions are equivilent to each other.
-nSigma m n p = map ((-) 1) [sigma m, sigma n, sigma p]
+nSigma m n p = parMapStrat ((-) 1) [sigma m, sigma n, sigma p]
 
 sigma1 (n, k) = sum $ factors (n^k)
 sigma2 (n, k) = (n^(k+1)-1) `div` (n-1)
@@ -23,7 +24,7 @@ sigmaProof = [ checks [sigma1, sigma2, sigma3] (n, k) | k <- [2..10], n <- prime
     where checks xs nK = [ check fs nK | fs <- uniquePairs xs ]
           check (f1, f2) nK = (f1 nK) == (f2 nK)
 
-uniquePairs xs = concat $ zipWith (map . (,)) xs (tail $ tails xs)
+uniquePairs xs = concat $ zipWith (parMapStrat . (,)) xs (tail $ tails xs)
 -- And you can check large lists with this.
 allTrue = all $ all (== True)
 -- Example:
@@ -32,28 +33,11 @@ allTrue = all $ all (== True)
 -- Et cetera.
 
 -- Parallel factors.
-factors   n = 1 : (factorsOf n) ++ [n]
+factors   n = 1 : (Main.factorsOf n) ++ [n]
 factorsOf n = let candidates = [2..n `div` 4]
-              in catMaybes $ map findMod candidates
-                  where findMod x = if n `mod` x == 0 then Just x else Nothing
+              in catMaybes $ parMapStrat findMod candidates
+                  where findMod x = if n `rem` x == 0 then Just x else Nothing
 
-paraSum = (parMap rseq) (sum . factors)
-
-primes :: Integral a => [a]
-primes = 2:3:5:(filter fastIsPrime $ candidates 7 11)
-    where candidates x y = x:y: candidates (x+6) (y+6)
-          fastIsPrime n = not $ any isFactor [3, 5 .. intRoot n]
-              where isFactor f = n `mod` f == 0
-                    intRoot    = floor . sqrt . fromIntegral
-
-isPrime :: (Integral a) => a -> Bool
-isPrime n
-    | n < 2                      = False
-    | n < 8 && (n == 2 || odd n) = True
-    | even n || isFactor 5       = False
-    | otherwise                  = not $ any isFactor [3, 5 .. root]
-    where
-        isFactor f = n `mod` f == 0
-        root       = (floor . sqrt . fromIntegral) n
+parMapStrat = parMap rseq
 
 main = do print amicablePairs

1ceeedb08e44a3c5a199aea341aefcaf48e84eeb
Zach Fogg <zach@zachfogg.com>
2011-12-28 04:59:53 -0600

    Touched up 28 once more. Think I'm almost done with it.

--- projectEuler28.hs
+++ projectEuler28.hs
@@ -4,9 +4,9 @@ import Data.List
 import Control.Parallel.Strategies
 import Euler
 
-sumDiagonals n = sum $ parallelMap (sum . diagonals) [0 .. nthOfSideLength]
+sumDiagonals n = sum $ 1 : parallelMap (sum . diagonals) [1 .. nthOfSideLength]
     where diagonals x     = everyNth (x*2) (nthSpiral x)
-          nthOfSideLength = (n+2) `div` 2 - 3
+          nthOfSideLength = (n+2) `div` 2 - 1
           parallelMap     = parMap rpar
 
 nthSpiral 0 = [1]
@@ -14,8 +14,6 @@ nthSpiral n = [ start .. end ]
     where start = 1 + last (nthSpiral (n - 1))
           end   = 1 + 8*triangleNumbers !! (n - 1)
 
-everyNth n xs = case drop (n-1) xs of
-                 (y:ys) -> y : everyNth n ys
-                 []     -> []
+everyNth n = map snd . filter ((== 0) . (`rem` n) . fst) . zip [1 .. ]
 
 main = do print $ sumDiagonals 1001

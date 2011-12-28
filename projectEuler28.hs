6f6dad856ededfdfe76f7075f48c3b35bec3330a
Zach Fogg <zach@zachfogg.com>
2011-12-28 02:58:37 -0600

    Reworked the code a bit to look better and make sense more, but no real functionality changes.

--- projectEuler28.hs
+++ projectEuler28.hs
@@ -4,16 +4,15 @@ import Data.List
 import Control.Parallel.Strategies
 import Euler
 
-sumDiagonals n = sum $ (parMap rpar) (sum . diagonals) [0 .. nthOfSideLength]
+sumDiagonals n = sum $ parallelMap (sum . diagonals) [0 .. nthOfSideLength]
     where diagonals x     = everyNth (x*2) (nthSpiral x)
           nthOfSideLength = (n+2) `div` 2 - 3
+          parallelMap     = parMap rpar
 
 nthSpiral 0 = [1]
-nthSpiral n = [
-                  1 + last (nthSpiral (n - 1))
-                  ..
-                  1 + 8*(triangleNumbers !! (n - 1))
-              ]
+nthSpiral n = [ start .. end ]
+    where start = 1 + last (nthSpiral (n - 1))
+          end   = 1 + 8*triangleNumbers !! (n - 1)
 
 everyNth n xs = case drop (n-1) xs of
                  (y:ys) -> y : everyNth n ys

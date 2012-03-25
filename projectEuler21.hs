edab0223234691425bb99ff4bbf04e67e639a548
Zach Fogg <zach@zachfogg.com>
2012-03-25 02:34:43 -0400

    Better formula for uniquePairs, fixed nSigma, rearranged sigmaN arguments, added comments about sigmas.

--- projectEuler21.hs
+++ projectEuler21.hs
@@ -1,21 +1,23 @@
 import Euler
 import Data.List
+import Data.Function
 import Control.Parallel
 
 amicablePairs = filter parIsAmic (uniquePairs [2..10^3])
     where parIsAmic (x, y) = (q `par` p) && (p `pseq` q)
              where amicable x y = sigma x - x == y
-                   p = amicable x y
-                   q = amicable y x
+                   (p, q) = (amicable x y, amicable y x)
 
 -- Euler's Sigmas.
-nSigma m n p = map ((-1) +) [sigma m, sigma n, sigma p]
+nSigma x k = map ((-1) +) [sigma1 x k, sigma2 x k, sigma3 x k]
 
-sigma n    = sum $ factors n
+sigma x    = sum $ factors x
 
-sigma1 k n = sum $ factors (n^k)
-sigma2 k n = sum $ 1 : [ n^x | x <- [1..k] ]
-sigma3 k n = (n^(k+1)-1) `div` (n-1)
+-- { s1 = s2 = s3   if x is prime
+--   s(x, k) = x+k  if x is prime and k is 1 }
+sigma1 x k = sum $ factors (x^k)
+sigma2 x k = sum $ 1 : map (x^) [1..k]
+sigma3 x k = (x^(k+1)-1) `div` (x-1)
 
 -- And here is a list of boolean results, as proof.
 -- This proof should return lists of True values.
@@ -23,7 +25,8 @@ sigmaProof = [ checks [sigma1, sigma2, sigma3] k n | k <- [2..10], n <- primes ]
     where checks xs k n = [ check fs k n | fs <- uniquePairs xs ]
               where check (f1, f2) k n = f1 k n == f2 k n
 
-uniquePairs xs = concat $ zipWith (map . (,)) xs (tail $ tails xs)
+uniquePairs xs = [ (head x, y) | x <- (init $ tails xs), y <- (tail x) ]
+
 -- And you can check large lists with this.
 allTrue = all and
 -- Example:
@@ -31,6 +34,6 @@ allTrue = all and
 
 -- Et cetera.
 
-factors n = 1 : divisors n ++ [n]
+factors x = 1 : divisors x ++ [x]
 
 main = print amicablePairs
